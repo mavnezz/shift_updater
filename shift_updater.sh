ed1ba2e58a0c0d7ffddc8cda096e68dea46b310f
mavnezz <julian@stuch.info>
2018-01-11 15:51:19 +0100

    Update shift_updater.sh

--- shift_updater.sh
+++ shift_updater.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Shift version to update to
-version=$(curl -s 'https://raw.githubusercontent.com/mavnezz/shift_updater/master/shift_version')
+version=$(curl -s 'https://raw.githubusercontent.com/ShiftNrg/shift/master/package.json' | awk -F=":" -v RS="," '$1~/"version"/ {print}')
+version=(`echo $version| tr ":" "\n"`)
+version=${version[1]}
+version=(`echo $version| tr --delete \" `)
+echo $version
+
 
 if [ -e $version ]
 then
     echo "Version $version allready installed"
 else
+    echo "Installing version $version"		
 	crontab -r
 	echo "* * * * * php ~/shift-checker/checkdelegate.php >> ~/shift-checker/logs/checkdelegate.log 2>&1" > cronjob.out
 	echo "0 1 * * * ./shift_updater.sh" >> cronjob.out
@@ -18,7 +24,8 @@ else
 	echo "Updating Wallet"
 	~/shift/shift_manager.bash update_wallet
 	echo "Shift updated to version $version"
-    echo $version > $version
+    	echo $version > $version
 	crontab cronjob.out
 fi
+
 
